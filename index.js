94a4b6a6dabc200a1b766aa0a15636880f262521
Stecarter11 <63299688+Stecarter11@users.noreply.github.com>
2020-04-07 14:39:31 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+let name ="Steven"
+let age = 42
+let jobRole ="Programme Manager"
+
+
+function personalData(name, age){
+    console.log("Hello, I am " + name + " and I am " + age + " years old,")
+
+}
+
+personalData ("Jacob", 44)
+personalData ("James", 35)
+personalData ("John", 41)
+
+let numberOne;
+let numberTwo;
+
+function addition(numberOne, numberTwo) {
+    let total = numberOne + numberTwo
+    console.log(total)
+
+}
+
+addition(50, 40)
+addition(30, 50)
